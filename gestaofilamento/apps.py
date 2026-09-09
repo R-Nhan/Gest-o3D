140dffe19d6572bd5f5106d7befda6e989689d76
R-Nhan <rubensiangbi@gmail.com>
2026-09-09 11:51:03 -0300

    feat: estruturacao de pastas

---
+++ gestaofilamento/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestaofilamentoConfig(AppConfig):
+    name = 'gestaofilamento'
---
+++ gestaoresina/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestaoresinaConfig(AppConfig):
+    name = 'gestaoresina'
