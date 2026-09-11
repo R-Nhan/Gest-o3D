26f275edd24f4f67de7de43fc3c5ba9f816f90d2
R-Nhan <rubensiangbi@gmail.com>
2026-09-11 17:30:46 -0300

    feat: tela de login pronta

--- INARCA/settings.py
+++ INARCA/settings.py
@@ -106,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'images',
+]
 
 
 # Email
